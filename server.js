0af794d78ede128ed672e8b54d903416a65eff77
ToavinaRazafy <toavinarazafy3@gmail.com>
2021-04-14 18:00:37 +0300

    cors

--- server.js
+++ server.js
@@ -28,12 +28,13 @@ mongoose.connect(uri, options)
         });
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next();
-});
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}));
 
 // use JWT auth to secure the api
 app.use(jwt());
